49ee5c21bb9eee32ebbbfb7212376a89d44edc57
bitflower <matthias.max@bitflower.net>
2020-01-30 15:11:34 +0100

    init

---
+++ src/global/app.ts
@@ -0,0 +1,11 @@
+import dotenvPlugin from "rollup-plugin-dotenv";
+
+export default async () => {
+  console.log(dotenvPlugin);
+
+  /**
+   * The code to be executed should be placed within a default function that is
+   * exported by the global script. Ensure all of the code in the global script
+   * is wrapped in the function() that is exported.
+   */
+};
